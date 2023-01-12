ebb65ffc9f3679639efb85974719259dd9de0e00
jcng <joshuang.contact@gmail.com>
2023-01-11 23:17:07 -0800

    Adding to garden description

--- src/pages/garden.js
+++ src/pages/garden.js
@@ -34,7 +34,13 @@ const GardenPage = ({ data }) => {
         What am I looking at?
       </h2>
       <p>
-        This isn't quite a blog, or a portfolio, or a tutorial site — it's a <strong>Digital Garden</strong>. You can think of it as a personal wiki, or an online notebook. Its a loose collection of ideas, notes, writings, and media, with no rules about what does or does not belong.
+        This isn't quite a blog, or a portfolio, or a tutorial site — it's a <strong>Digital Garden</strong>. You can think of it as something like a personal wiki, or an online notebook. It's a loose collection of ideas, with no rules about what does or does not belong. Whereas a blog is a chronologically sorted collection of "completed" posts, a digital garden is composed of various connected pieces (of writing or really any other media that can be presented in a browser) in any stage of completeness. Some are scraps of ideas, others are robust projects. You'll also find that there's no particular theme to what's found here.
+      </p>
+      <p>
+        I definitely did not come up with this idea myself! I first learned of this in a tweet from <a href="https://twitter.com/Mappletons">Maggie Appleton</a>, who has a great writeup on Digital Gardens if you're interested in the concept: <a href="https://maggieappleton.com/garden-history">A Brief History & Ethos of the Digital Garden</a>.
+      </p>
+      <p>
+        See <a href="/about-the-garden">About the Garden</a> for more on my personal reasons for creating this, as well as the nitty gritty of how it's built.
       </p>
       <div id="garden-post-list">
         {data.allMarkdownRemark.edges.map(({ node }) => (
