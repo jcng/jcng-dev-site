2caa5531f15fbaab1c73295c969860fd534eaba8
jcng <joshuang.contact@gmail.com>
2023-04-23 01:41:15 -0700

    update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,9 @@ import Layout from '../components/layout'
 const AboutPage = () => {
     return (
         <Layout pageTitle="About Me">
-            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+            <p>Hi there, I'm Josh Ng!</p>
+            <p><strong>Professionaly</strong>: A series of tech job titles that are hard to explain to relatives at Thanksgiving dinner. Bachelor of Science from UC Irvine for Informatics, Human Computer Interaction specialization. It's also hard to explain.</p>
+            <p><strong>Not so professionally: </strong>Unskilled at (but very enthusiastic about) powerlifting, photography, music, history.</p>
         </Layout>
     )
 }
