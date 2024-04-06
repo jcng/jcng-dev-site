770d6d998310a400dbf372b098c3cfe254ad50e4
jcng <joshuang.contact@gmail.com>
2024-04-06 10:06:08 -0700

    additional styling

---
+++ src/pages/books.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const BooksPage = () => {
+    return (
+        <Layout pageTitle="Books I've Read">
+            <p>...</p>
+        </Layout>
+    )
+}
+
+export default BooksPage
